79a095f52e4bad646f8819d5c5006c2dd0d169d2
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-07-18 14:16:58 +0530

    created basic video model

---
+++ application/models/Video_model.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Video_model extends CI_Model {
+
+	public function player($videoid) {
+
+    }
+
+    public function get($id) {
+
+    }
+
+    public function get_by_category($category_id) {
+
+    }
+
+    public function create() {
+
+    }
+
+    public function add_category($id, $category_id) {
+
+    }
+
+    public function delete($id) {
+
+    }
+}
