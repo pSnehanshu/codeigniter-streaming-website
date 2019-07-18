13d8f665ebb5b867d758848b5141e22758937d97
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-07-18 13:22:28 +0530

    created basic layout

---
+++ application/views/layouts/footer.php
@@ -0,0 +1,13 @@
+
+    </main>
+    <!-- main end -->
+
+    <!-- FOOTER -->
+    <footer class="container">
+        <p class="float-right"><a href="#">Back to top</a></p>
+        <p>&copy; Lendou Group &middot; <a href="<?=site_url('home/page/privacy')?>">Privacy</a> &middot; <a href="<?=site_url('home/page/terms')?>">Terms</a></p>
+    </footer>
+    <!-- FOOTER end -->
+</body>
+
+</html>
