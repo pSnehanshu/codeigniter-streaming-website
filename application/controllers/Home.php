50c2173b4053dc68f2622a3fc8f63fc2fb534428
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-07-20 20:00:13 +0530

    Home/category working good

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -11,9 +11,17 @@ class Home extends CI_Controller {
 	}
 
 	public function category($catslug = 'all') {
-		//$this->load->view('layouts/header');
-		echo '<h1>Category ' . $catslug . '</h1>';
-		//$this->load->view('layouts/footer');
+		$this->load->model('Object_model');
+		$objects = $this->Object_model->get_children($catslug);
+
+		$this->load->view('layouts/header');
+		$this->load->view('category', array(
+			'objects' => $objects,
+			'slug' => $catslug,
+			'ref' => explode('/', $this->input->get('ref')),
+			'next_ref' => $this->input->get('ref').'/'.$catslug
+		));
+		$this->load->view('layouts/footer');
 	}
 
 	public function page($pageslug = 'all') {
