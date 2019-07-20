ab93ff26d5db66314ee64b09927dd762202fe500
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-07-20 21:09:36 +0530

    sorting category children based on child_order

--- application/models/Object_model.php
+++ application/models/Object_model.php
@@ -10,11 +10,13 @@ class Object_model extends CI_Model {
             return null;
     }
 
+
 	public function get_children($slug, $start = 0, $num = 10) {
         $this->db->select('objects.*');
         $this->db->from('objects, object_heirarchy');
         $this->db->where('object_heirarchy.parent = (SELECT id FROM objects WHERE slug = '.$this->db->escape($slug).')');
         $this->db->where('object_heirarchy.child = objects.id');
+        $this->db->order_by('object_heirarchy.child_order', 'ASC');
         $query = $this->db->get();
         return $query->result();
     }
