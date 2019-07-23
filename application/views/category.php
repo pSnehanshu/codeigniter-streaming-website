e1b5d3846c16a6c8534922e495eb76a1b35631f8
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-07-23 18:20:19 +0530

    styled the category page a lot

--- application/views/category.php
+++ application/views/category.php
@@ -1,3 +1,27 @@
+<style>
+.center-title{
+    text-align: center;
+}
+.thumbnail-holder .show-on-hover{
+    opacity: 0;
+    transition: opacity 0.2s linear;
+}
+.thumbnail-holder:hover .show-on-hover{
+    opacity: 1;
+}
+.object-type, .object-title{
+    position: absolute;
+}
+.object-type{
+    width: auto;
+    font-size: 12px;
+    border-bottom-right-radius: 5px;
+}
+.object-title{
+    bottom: 2px;
+    width: 100%;
+}
+</style>
 <div class="container-fluid py-3">
     <?php // Breadcrumb ?>
     <nav aria-label="breadcrumb">
@@ -18,12 +42,15 @@
     <div class="d-flex">
 
         <?php foreach ($objects as $object) : ?>
-            <div class="p-2 grow" style="max-width: 190px;">
-                <a href="<?= site_url('home/goto/' . $object->id . '?ref=' . urlencode($next_ref)) ?>">
-                    <img src="<?= $object->thumbnail ?>" style="max-height:200px;" class="img-thumbnail"><br>
-                    <p>[<?=strtoupper($object->type)?>] <?= $object->title ?></p>
-                </a>
+        <a href="<?= site_url('home/goto/' . $object->id . '?ref=' . urlencode($next_ref)) ?>">
+            <div class="p-2 grow center-title" style="max-width: 190px;">
+                <div class="thumbnail-holder position-relative">
+                    <div class="object-type bg-light text-dark border px-2 show-on-hover"><?=strtolower($object->type)?></div>
+                    <div class="object-title bg-light text-dark border px-3 show-on-hover"><?= $object->title ?></div>
+                    <img src="<?= $object->thumbnail ?>" style="max-height:200px;" class="img-thumbnail">
+                </div>
             </div>
+        </a>
         <?php endforeach; ?>
     </div>
 </div>
