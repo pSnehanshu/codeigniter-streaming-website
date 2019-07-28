5f489f08bf80d952bf861fb06a779e278840e181
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-07-28 13:01:07 +0530

    implemented dynamic browsing of seasons

--- application/views/show.php
+++ application/views/show.php
@@ -21,11 +21,9 @@
 ?>
 <div class="container-fluid bg-dark text-light px-5">
     <nav class="nav justify-content-center">
-        <?php $i = 1;
-        foreach ($seasons as $season) : ?>
-            <a class="nav-link text-light" href="?seasonid=<?= $season->id ?>">Season <?= $i ?></a>
-            <?php $i++;
-        endforeach; ?>
+        <?php foreach ($seasons as $season) : ?>
+            <a class="season-link nav-link <?= ($selected_season == $season->id ? 'text-dark bg-light' : 'text-light') ?>" href="?seasonid=<?= $season->id ?>" data-id="<?=$season->id?>">Season <?= $season->object_order ?></a>
+        <?php endforeach; ?>
     </nav>
 </div>
 
@@ -41,10 +39,33 @@
 
 
 <script>
-$(function (){
-    <?php if ($selected_season):?>
-    $('#episodes-holder').load("<?=site_url('home/season_ajax/'.$selected_season)?>");
-    <?php endif;?>
+    $(function() {
+        <?php if ($selected_season) : ?>
+            $('#episodes-holder').load("<?= site_url('home/season_ajax/' . $selected_season) ?>");
+        <?php endif; ?>
 
-});
+    });
+    $('.season-link').click(function (e) {
+        e.preventDefault();
+        let seasonId = $(this).attr('data-id');
+        activateSeasonLink(seasonId);
+        $('#episodes-holder').html(`
+            <div class="text-center">
+                <div class="spinner-border" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>
+        `);
+        $('#episodes-holder').load("<?= site_url('home/season_ajax/') ?>" + seasonId);
+    });
+
+    function activateSeasonLink(activeId = 0) {
+        $('.season-link').each(function (i) {
+            if ($(this).attr('data-id') == activeId) {
+                $(this).removeClass('text-light').addClass('text-dark bg-light');
+            } else {
+                $(this).addClass('text-light').removeClass('text-dark bg-light');
+            }
+        });
+    }
 </script>
