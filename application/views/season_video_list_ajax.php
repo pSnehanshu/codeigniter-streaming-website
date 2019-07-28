24ddba5a6c21bb7bc034836c23a79542aa356a9f
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-07-28 13:11:19 +0530

    episode list showing message when no videos

--- application/views/season_video_list_ajax.php
+++ application/views/season_video_list_ajax.php
@@ -1,16 +1,20 @@
-<?php foreach ($videos as $video):?>
+<?php if (count($videos) > 0):?>
+    <?php foreach ($videos as $video):?>
 
-<a href="<?=site_url('home/watch/'.$video->slug)?>">
-<div class="row mt-2 grow">
-    <div class="col-3">
-        <img src="<?=htmlentities($video->thumbnail)?>" alt="<?=htmlentities($video->title)?>" class="img-thumbnail" style="width: 300px;">
+    <a href="<?=site_url('home/watch/'.$video->slug)?>">
+    <div class="row mt-2 grow">
+        <div class="col-3">
+            <img src="<?=htmlentities($video->thumbnail)?>" alt="<?=htmlentities($video->title)?>" class="img-thumbnail" style="width: 300px;">
+        </div>
+        <div class="col-9">
+            <h2>
+                <?=htmlentities($video->title)?>
+            </h2>
+            <p class="text-dark"><?=htmlentities($video->description)?></p>
+        </div>
     </div>
-    <div class="col-9">
-        <h2>
-            <?=htmlentities($video->title)?>
-        </h2>
-        <p class="text-dark"><?=htmlentities($video->description)?></p>
-    </div>
-</div>
-</a>
-<?php endforeach;?>
+    </a>
+    <?php endforeach;?>
+<?php else:?>
+    <p class="text-center">Oops! No videos for now. Keep looking for the infinity stones.</p>
+<?php endif;?>
