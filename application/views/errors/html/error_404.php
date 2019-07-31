2881c41a636035d0dbc2999ed9bfb9254410d568
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-07-31 13:36:05 +0530

    updated logo and favicon links with cloudfront

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -15,8 +15,8 @@ $CI->load->helper('url');
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="shortcut icon" href="https://eimiflix-videos-sp-741100443704-ap-south-1.s3.ap-south-1.amazonaws.com/free/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="https://eimiflix-videos-sp-741100443704-ap-south-1.s3.ap-south-1.amazonaws.com/free/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="https://dglyi7b99dtt4.cloudfront.net/free/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="https://dglyi7b99dtt4.cloudfront.net/free/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<title>404 Page not found | Eimiflix.com</title>
 	<style>#main { height: 100vh; }</style>
@@ -27,7 +27,7 @@ $CI->load->helper('url');
 		<h1 class="mr-3 px-3 align-top border-right inline-block align-content-center">404 page not found</h1>
 		<div class="inline-block align-middle px-2">
 			<a href="<?=site_url()?>">
-				<img style="max-width: 80%;" src="https://eimiflix-videos-sp-741100443704-ap-south-1.s3.ap-south-1.amazonaws.com/free/main-logo.png" alt="Eimiflix">
+				<img style="max-width: 80%;" src="https://dglyi7b99dtt4.cloudfront.net/free/favicon.ico" alt="Eimiflix">
 			</a>
 		</div>
 	</div>
