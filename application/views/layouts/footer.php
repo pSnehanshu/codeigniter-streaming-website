fb5f0ca7ec083a516808ff2de30704de548564d8
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-08-04 21:12:57 +0530

    back to top link removed

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -7,7 +7,6 @@
     <!-- FOOTER -->
     <footer class="mt-auto">
         <div class="container">
-            <p class="float-right"><a href="#">Back to top</a></p>
             <p>&copy; <?=date('Y')?>, Lendou Group &middot; <a href="<?=site_url('home/page/privacy')?>">Privacy</a> &middot; <a href="<?=site_url('home/page/terms')?>">Terms</a></p>
         </div>
     </footer>
