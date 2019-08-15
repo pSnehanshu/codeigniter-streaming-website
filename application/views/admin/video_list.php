a4bf1fa276793c50a99bd31e8204618c080ec793
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-08-16 02:08:24 +0530

    highlighting unpulbished videos

--- application/views/admin/video_list.php
+++ application/views/admin/video_list.php
@@ -1,6 +1,6 @@
 <div>
     <div class="w3-panel w3-right">
-        <a class="w3-btn w3-border w3-round w3-blue" href="<?=site_url('admin/video')?>">Add new video</a>
+        <a class="w3-btn w3-border w3-round w3-blue" href="<?= site_url('admin/video') ?>">Add new video</a>
     </div>
     <table class="w3-table w3-striped">
         <thead>
@@ -12,22 +12,24 @@
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($videos as $video):?>
+            <?php foreach ($videos as $video) : ?>
 
-            <tr>
+            <tr style="opacity: <?= $video->is_published == 1 ? '1' : '0.4' ?>">
                 <td>
-                    <a href="<?=site_url('admin/video/'.$video->id)?>">
-                        <img src="<?=htmlentities($video->thumbnail)?>" width="100">
+                    <a href="<?= site_url('admin/video/' . $video->id) ?>">
+                        <img src="<?= htmlentities($video->thumbnail) ?>" width="100">
                     </a>
                 </td>
                 <td>
-                    <a href="<?=site_url('admin/video/'.$video->id)?>"><?=htmlentities($video->title)?></a>
+                    <a href="<?= site_url('admin/video/' . $video->id) ?>"><?= htmlentities($video->title) ?></a>
                 </td>
-                <td><div class="<?=$video->is_premium == 1 ? 'w3-text-green' : 'w3-text-red'?>"><?=$video->is_premium == 1 ? 'Yes' : 'No'?></div></td>
-                <td><?=$video->created_on?></td>
+                <td>
+                    <div class="<?= $video->is_premium == 1 ? 'w3-text-green' : 'w3-text-red' ?>"><?= $video->is_premium == 1 ? 'Yes' : 'No' ?></div>
+                </td>
+                <td><?= $video->created_on ?></td>
             </tr>
 
-            <?php endforeach;?>
+            <?php endforeach; ?>
         </tbody>
     </table>
 </div>
